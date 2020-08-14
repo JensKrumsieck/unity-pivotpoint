150c0d1257a6b10852f709319f73454d2b397692
Jens Krumsieck <jenso666@live.de>
2020-08-14 17:06:13 +0200

    init

---
+++ Cursor3D.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+public class Cursor3D : EditorWindow
+{
+
+    [MenuItem("Window/3DCursor/Enable #Y")]
+    public static void Enable()
+    {
+        SceneView.duringSceneGui += OnScene;
+    }
+
+    [MenuItem("Window/3DCursor/Disable #%Y")]
+    public static void Disable()
+    {
+        SceneView.duringSceneGui -= OnScene;
+    }
+
+    [MenuItem("Window/3DCursor/Reset #X")]
+    public static void ResetPivot()
+    {
+        var scene = SceneView.lastActiveSceneView;
+        scene.pivot = Vector3.zero;
+    }
+
+    private static void OnScene(SceneView obj)
+    {
+        var scene = SceneView.lastActiveSceneView;
+        Handles.color = Handles.selectedColor;
+        Handles.SphereHandleCap(0, scene.pivot, Quaternion.identity,.2f, EventType.Repaint);
+        scene.Repaint();
+    }
+
+}
+
+#endif
---
+++ README.md
@@ -0,0 +1,7 @@
+# Unity Pivot Point Tool
+
+Adds a Menu Item into the Unity Editor to Control the Scene-Pivot Point.
+This functionality is inspired by the 3DCursor-Feature from blender.
+
+You can Enable & Disable a "Gizmo" to show the Pivot Point with `Shift + Y`.
+`Shift + X` resets the Scene's Pivot Point to (0,0,0)!
